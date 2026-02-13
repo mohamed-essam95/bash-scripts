7875b2fb68bc97bd8585f3dafcc21df051dc7875
mohamed-essam95 <mohamed.essam955559@gmail.com>
2026-02-13 04:17:05 +0200

    add loop scripts: for, until, and while examples

---
+++ for_loop.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+for fruit in apple banana mango; do
+    echo "I like $fruit"
+done
---
+++ while_loop.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+count=1
+
+while [ $count -le 5 ]; do
+    echo "Count is: $count"
+    ((count++))
+done
