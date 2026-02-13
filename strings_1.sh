52076e2463b80b758849525eaaa4f89a00bee1c9
mohamed-essam95 <mohamed.essam955559@gmail.com>
2026-02-13 05:16:29 +0200

    add strings_1.sh and strings_2.sh scripts for string manipulation examples

---
+++ strings_1.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+string="Hello World"
+echo ${#string}
---
+++ strings_2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+string="HelloWorld"
+
+echo ${string:0:5} # Output: Hello
+echo ${string:5} # Output: World
+echo ${string:2:3} # Output: llo
