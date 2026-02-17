c11bad26a8a8d335649f1bba9b2373d86c3895b6
mohamed-essam95 <mohamed.essam955559@gmail.com>
2026-02-17 02:05:14 +0200

    refactor user_profile.sh to improve variable usage and maintain consistency

--- assigment/user_profile.sh
+++ assigment/user_profile.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
+
+current_year=$(date +%Y)
+
 read -p "Enter your first name: " first_name
 read -p "Enter your last name: " last_name
 read -p "Enter your age: " age
-curt_year=$(date +%Y)
-birth_year=$((curt_year - age))
+
+current_time=$(date)
+
+birth_year=$((current_year - age))
 echo "Hello, $first_name $last_name ,You were born in $birth_year."
+echo "Current date and time: $current_time"
